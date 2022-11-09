c949e44b8759d96da1b948759a5444d3c9b94bfd
XiaoyaWang-gh <57889612+XiaoyaWang-gh@users.noreply.github.com>
2022-11-09 18:15:10 +0800

    这个java文件的作用是对数据集进行处理，主要做了两件事
    1. 将测试用例分成label分别为0，1，2的三类，并将0类写入一个新文件(含id)保存起来。
    2. 将0类的prefix和oracle分离，分别(含id)保存起来。
    所谓1类，过长的oracle部分。
    所谓2类，prefix和oracle交错穿插出现。
    三者的数量分别是
    0 : 503751个测试用例
    1 : 49389个测试用例
    2 : 70882个测试用例

---
+++ processTC.java
@@ -0,0 +1,111 @@
+package processM2T;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class processTC {
+	
+	private final static String add  ="C:\\codes\\Java\\processM2T\\output.tests.txt";
+	private final static String label0add  ="C:\\codes\\Java\\processM2T\\output.tests.label0.txt";
+	private static BufferedReader br;
+	private static BufferedWriter bw;
+	
+	public static int getAssertNum(String testcase){
+		int assert_num = 0;
+		String target = "assert";
+		int idx=-2;
+		while(idx!=-1) {
+			idx = testcase.indexOf(target, Math.max(0, idx+1));
+			if(idx>=0)assert_num++;
+		}
+		return assert_num;
+	}
+	
+	public static ArrayList<Integer> getAssertIdxArr(String testcase) {
+		ArrayList<Integer> idxArr = new ArrayList<Integer>();
+		String target = "assert";
+		int idx=-2;
+		while(idx!=-1) {
+			idx = testcase.indexOf(target, Math.max(0, idx+1));
+			if(idx>=0)idxArr.add(idx);
+		}
+		
+		return idxArr;
+	}
+	
+	public static boolean isInterLaced(String testcase) {
+
+		String target = "assert";		
+		//现在就差把上面的思维泛化了
+		ArrayList<Integer> idxArr  = getAssertIdxArr(testcase);
+		for(int i=0;i<idxArr.size();i++) {
+			int idx = idxArr.get(i).intValue();
+			int idx1 = testcase.indexOf(";",idx);
+			int idx2 = testcase.indexOf(target,idx1);
+			if(idx == -1) {
+				continue;
+			}else {
+				if(idx2-idx1>13) {
+					return true;
+				}else {
+					continue;
+				}
+			}
+		}
+		
+		return false;
+	} 
+	
+	public static int getLabel(String testcase) {
+		int label = 0;
+		
+		//鉴定是否为类型1
+		if(getAssertNum(testcase)>5)return 1;
+		
+		//鉴定是否为类型2
+		if(isInterLaced(testcase))return 2;
+		
+		return label;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		long lineNum = Files.lines(Paths.get(new File(add).getPath())).count();
+		System.out.println("Total number of lines : " + lineNum);
+		
+		br = new BufferedReader(new FileReader(add), 310);
+		
+		
+		String line;
+		StringBuffer sb= new StringBuffer("");
+		
+		int labelArr[] = new int[] {0,0,0};
+	    while ((line = br.readLine()) != null) {
+	    	labelArr[getLabel(line)]++;
+	    	if(getLabel(line)==0)
+	    		sb.append(line+"\n");
+	    }
+	    br.close();
+
+		for(int j=0;j<3;j++) {
+			System.out.print(j);
+			System.out.print(" : ");
+			System.out.print(labelArr[j]);
+			System.out.print("个测试用例\n");
+		}
+		
+		bw = new BufferedWriter(new FileWriter(label0add),300);
+		bw.write(sb.toString());
+		bw.close();
+		System.out.println();
+		System.out.println("已将label==0的测试用例写入新文件");
+	}
+}
