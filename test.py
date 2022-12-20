b4466df4e9f5cc9b1329c3a34d289e40c431a581
XiaoyaWang-gh <57889612+XiaoyaWang-gh@users.noreply.github.com>
2022-12-20 18:25:09 +0800

    最终还是需要调用model.generate()方法来生成比较像样的测试用例

--- test.py
+++ test.py
@@ -1,25 +1,34 @@
 import path
 import torch as t
 from transformers import AutoConfig, AutoTokenizer
-
-from someshots import shotDict
 from plbartCopyGenerator import PLBartCopyGenerator
 
+#每个defects4j中的项目作为一个.txt文件
+file_path = path.pro_lang
 
 #加载模型
-config = AutoConfig.from_pretrained(path.my_checkpoint_copygen_1216)
-model = PLBartCopyGenerator.from_pretrained(path.my_checkpoint_copygen_1216,config=config)
+trained_path = path.my_ckpt_epoch_6
+config = AutoConfig.from_pretrained(trained_path)
+model = PLBartCopyGenerator.from_pretrained(trained_path,config=config)
 #加载分词器
 tokenizer = AutoTokenizer.from_pretrained(path.hf_checkpoint)
 
 
-def test_model(input):
-    
+# 读取d4j中的focal methods,key是项目名称+缺陷序号
+def read_file()->dict():
+    langDict = dict()
+    with open(file_path,"r") as file:
+        for line in file:
+            key, value = line.split(',',1)
+            langDict[key] = value
+    return langDict
+
+def cut_seq(input) -> str:
     if len(input) > 1024:
         input = input[:1024]
-    else:
-        input = input[:len(input)]
+    return input
 
+def test_model(input): # 不用Generate
     tokenized_input = tokenizer(input,add_special_tokens=False,return_tensors="pt")
     input_ids = tokenized_input.input_ids
     seq2seqLMOutput = model(input_ids)
@@ -28,13 +37,33 @@ def test_model(input):
     outputs = tokenizer.batch_decode(output_tokens, skip_special_tokens=True)
 
     print(outputs[0])
+
+def test_model_gen(input): # 使用Generate()
+    tokenized_input = tokenizer(input,add_special_tokens=False,return_tensors="pt")
+    input_ids = tokenized_input.input_ids
+    # 对生成做要求
+    gen_kwargs = {
+        "no_repeat_ngram_size":3,
+        "max_length":200,
+        "min_length":40,
+    }
+    outputs = model.generate(inputs=input_ids,**gen_kwargs)
+    output_tokens = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    print(output_tokens[0])
+    pass
  
 
 def main():
-    for k,v in shotDict.items():
-        print(k,"test prefix:")
-        test_model(v)
-        print()
+    langDict = read_file()
+    for k,v in langDict.items():
+        print(k,"😃")
+        print("focal method 👇\n",v)
+        print("focal method 👆")
+        print("test prefix 👇")
+        v = cut_seq(v)
+        test_model_gen(v)
+        print("test prefix 👆 ")
+
 
 if __name__ == "__main__":
     main()
